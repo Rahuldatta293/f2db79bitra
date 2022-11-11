f36ba6280c6bc29ea09f9b22e939ad1acc0759af
Rahul datta <bitrarahuldatta@gmail.com>
2022-11-11 01:42:11 -0600

    resource to update

--- routes/iphone.js
+++ routes/iphone.js
@@ -2,6 +2,9 @@ var express = require('express');
 const iphone_controlers= require('../controllers/iphone');
 var router = express.Router();
 
+ // GET request for one costume.
+ router.get('/iphone/:id', iphone_controlers.iphone_detail);
+ 
 /* GET home page. */
 router.get('/', iphone_controlers.iphone_view_all_Page);
 
