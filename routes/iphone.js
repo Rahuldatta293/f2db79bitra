69b2f57e4a4d2d1282a308e395f74098d7ae32b1
Rahul datta <bitrarahuldatta@gmail.com>
2022-11-16 22:47:15 -0600

    Added Bonus Part

--- routes/iphone.js
+++ routes/iphone.js
@@ -11,5 +11,14 @@ router.get('/', iphone_controlers.iphone_view_all_Page);
 /* GET detail costume page */
 router.get('/detail', iphone_controlers.iphone_view_one_Page);
 
+/* GET create costume page */
+router.get('/create', iphone_controlers.iphone_create_Page);
+
+/* GET create update page */
+router.get('/update', iphone_controlers.iphone_update_Page);
+
+/* GET delete costume page */
+router.get('/delete', iphone_controlers.iphone_delete_Page);
+
 module.exports = router;
 
