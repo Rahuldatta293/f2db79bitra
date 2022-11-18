0a23913e40b5c56a5f3cbfdbc210943a7f001ad9
Rahul datta <bitrarahuldatta@gmail.com>
2022-11-18 00:20:14 -0600

    passport section for authentication

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var Iphone = require("./models/iphone");
-
-
-
-
 
 require('dotenv').config();
 const connectionString =
@@ -24,6 +22,13 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
 db.once("open", function(){
 console.log("Connection to DB succeeded")});
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // We can seed the collection if needed on server start
 async function recreateDB(){
@@ -55,8 +60,6 @@ await Iphone.deleteMany();
   
   let reseed = true;
   if (reseed) { recreateDB();}
-  
-  
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -73,12 +76,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // We can seed the collection if needed on
-
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -87,6 +95,19 @@ app.use('/iphone', iphoneRouter);
 app.use('/gridbuild', gridbuildRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
